e412ff8ae91310fb18a06750f9852e5cb96656f2
Yonatan Reicher <yony252525@gmail.com>
2024-12-15 08:53:35 +0200

    Improve aesthetics

--- src/app/patients/ViewPatient.tsx
+++ src/app/patients/ViewPatient.tsx
@@ -19,7 +19,7 @@ function padTwoDigits (num: number): string {
 }
 
 const AppointmentDate: React.FC<{ date: Date }> = ({ date }) => {
-  return `${padTwoDigits(date.getDate())}-${padTwoDigits(date.getMonth())}-${date.getFullYear()}`
+  return `${date.getFullYear()}-${padTwoDigits(date.getMonth())}-${padTwoDigits(date.getDate())}`
 }
 
 const AppointmentTime: React.FC<{ date: Date }> = ({ date }) => {
@@ -41,17 +41,23 @@ const ViewAppointment: React.FC<{ appointment: Appointment, session: Session }> 
 
   // Paper is like a div with a shadow and a border.
   // TODO: What if the end_timee is on a different day?
-  return <Box component="span">
-    <AppointmentDate date={startTime} /> <AppointmentTime date={startTime} /> - <AppointmentTime date={endTime} />
-    <Paper
-      shadow="md"
-      mb="xs"
-      withBorder
-      bg="lime"
-    >
-      {doctorName ?? (error ? 'Could not load doctor' : 'Loading name...')}
-    </Paper>
-  </Box>
+  return <Paper
+    shadow="md"
+    mb="xs"
+    bg="green"
+    w="100%"
+    display="inline-block"
+    pl="xs"
+    pr="xs"
+    c="black"
+  >
+    <Group>
+      <AppointmentDate date={startTime} /> <AppointmentTime date={startTime} />-<AppointmentTime date={endTime} />: 
+      <Text m="auto">
+        {doctorName ?? (error ? 'Could not load doctor' : 'Loading name...')}
+      </Text>
+    </Group>
+  </Paper>
 }
 
 /**
@@ -78,6 +84,7 @@ const ViewPatientAppointments: React.FC<{ patientId: number, session: Session }>
   }
 
   return <Box>
+    <Text><strong>Appointments:</strong></Text>
     {appointments.map(appointment =>
       <ViewAppointment appointment={appointment} session={session} key={appointment.id} />
     )}
